3595c629cec5a5e4a88a885b983dc6468317dac1
moudida2023 <moudida2023@gmail.com>
2026-04-13 06:35:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from concurrent.futures import ThreadPoolExecutor
 # ⚙️ إعدادات الاتصال (بياناتك الشخصية)
 # ==========================================
 TELEGRAM_TOKEN = '8439548325:AAHOBBHy7EwcX3J5neIaf6iJuSjyGJCuZ68'
-CHAT_ID = '5067771509'
+CHAT_ID = '-1003692815602'
 
 # ==========================================
 # 📊 إدارة المخاطر والمحفظة
